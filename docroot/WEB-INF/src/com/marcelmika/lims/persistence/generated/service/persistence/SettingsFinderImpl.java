e1aeae9bd8d69c87c69f9d613ee235fb926414ef
marcelmika <marcel.mika@gmail.com>
2014-11-02 20:45:11 +0100

    #69 license regen

--- docroot/WEB-INF/src/com/marcelmika/lims/persistence/generated/service/persistence/SettingsFinderImpl.java
+++ docroot/WEB-INF/src/com/marcelmika/lims/persistence/generated/service/persistence/SettingsFinderImpl.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2014 Marcel Mika, marcelmika.com - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ *
+ * Written by Marcel Mika <marcelmika.com>, 2014
+ */
+
 package com.marcelmika.lims.persistence.generated.service.persistence;
 
 import com.liferay.portal.kernel.dao.orm.*;
