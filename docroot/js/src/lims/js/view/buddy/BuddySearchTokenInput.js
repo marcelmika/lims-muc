3501a4761915e545a9c07842d422138a7ec810ee
marcelmika <marcel.mika@gmail.com>
2015-05-06 15:00:19 +0200

    #353 autocomplete fix

--- docroot/js/src/lims/js/view/buddy/BuddySearchTokenInput.js
+++ docroot/js/src/lims/js/view/buddy/BuddySearchTokenInput.js
@@ -284,7 +284,6 @@ Y.LIMS.View.BuddySearchTokenInput = Y.Base.create('buddySearchTokenInput', Y.Vie
                 // Vars
                 var container = this.get('container'),
                     instance = this,
-                    autoCompleteContainer,
                     autoComplete = new Y.AutoComplete({
                         inputNode: this.get('tokenInputNode'),
                         resultTextLocator: 'fullName',
@@ -298,7 +297,7 @@ Y.LIMS.View.BuddySearchTokenInput = Y.Base.create('buddySearchTokenInput', Y.Vie
                         },
                         requestTemplate: this.get('requestTemplate'),
                         source: this.getServerRequestUrl(),
-                        render: true,
+                        render: false,
                         resultFilters: function (query, results) {
 
                             // Vars
@@ -312,13 +311,8 @@ Y.LIMS.View.BuddySearchTokenInput = Y.Base.create('buddySearchTokenInput', Y.Vie
                         }
                     });
 
-                // Get the autocomplete container
-                autoCompleteContainer = container.one('.yui3-aclist');
-                // We need to move the container a bit higher in the hierarchy, otherwise it
-                // would be overlapped by the upper elements
-                autoCompleteContainer.remove();
-                // Now we added to the desired container
-                container.append(autoCompleteContainer);
+                // Render it to container
+                autoComplete.render(container);
 
                 return autoComplete;
             }
